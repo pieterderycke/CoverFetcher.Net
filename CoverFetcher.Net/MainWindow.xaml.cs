b34b4aec67548a79d1d27de863f59dcfd6018355
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2014-01-04 17:45:56 +0100

    Project creation

---
+++ CoverFetcher.Net/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoverFetcher.Net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            string filename = (string)((DataObject)e.Data).GetFileDropList()[0];
+
+            TagLib.File file = TagLib.File.Create(filename);
+            string artist = file.Tag.FirstPerformer;
+            string albumArtist = file.Tag.FirstAlbumArtist;
+            string title = file.Tag.Title;
+            string album = file.Tag.Album;
+        }
+    }
+}
