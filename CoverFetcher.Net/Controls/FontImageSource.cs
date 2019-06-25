53a8ba254fc5d557045b48dfa9d370a0026568d6
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2019-06-25 09:23:45 +0200

    switched to a Segoe MDL2 icon for the drop zone

---
+++ CoverFetcher.Net/Controls/FontImageSource.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Media;
+//using System.Windows.Media.Composition;
+
+//namespace CoverFetcher.Controls
+//{
+//    public class FontImageSource : ImageSource
+//    {
+//        public override double Width => throw new NotImplementedException();
+
+//        public override double Height => throw new NotImplementedException();
+
+//        public override ImageMetadata Metadata => throw new NotImplementedException();
+
+//        protected override Freezable CreateInstanceCore()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal override ResourceHandle AddRefOnChannelCore(Channel channel)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal override Channel GetChannelCore(int index)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal override int GetChannelCountCore()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal override ResourceHandle GetHandleCore(Channel channel)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal override void ReleaseOnChannelCore(Channel channel)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
