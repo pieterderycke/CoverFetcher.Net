e83f3af7fc3a052614f9014fb3119a2bcf0b4856
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2020-02-29 10:56:53 +0100

    fixed the build script

--- buildZipPackage.cmd
+++ buildZipPackage.cmd
@@ -1,6 +1,9 @@
-SET version=0.2
+SET version=0.3
 
-msbuild /p:Configuration=Release CoverFetcher.Net\CoverFetcher.Net.csproj /t:Clean;Rebuild
+FOR /F "tokens=*" %%i IN ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') do (SET msbuild=%%i)
+
+Call "%msbuild%" /p:Configuration=Release CoverFetcher.Net\CoverFetcher.Net.csproj /t:Clean;Rebuild
+
 MKDIR zip
 COPY CoverFetcher.Net\bin\Release\*.exe zip\
 COPY CoverFetcher.Net\bin\Release\*.dll zip\
@@ -9,7 +12,6 @@ COPY CoverFetcher.Net\bin\Release\*.config zip\
 COPY LICENSE.md zip\
 COPY README.md zip\
 
-RM Jace.%version%.zip
 Tools\7-Zip\7za.exe a -tzip CoverFetcher_v%version%.zip .\zip\*
 
 RMDIR zip /S /Q
